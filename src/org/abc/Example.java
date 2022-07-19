c0fdcc90a81d4980dd6bb2fd54c40ccabcc43b5a
TharaQueen <tharanip98@gmail.com>
2022-07-19 23:46:50 +0530

    Code Entered

---
+++ src/org/abc/Example.java
@@ -0,0 +1,15 @@
+package org.abc;
+
+public class Example {
+	public static void main(String[] args) {
+		
+		int a=90;
+		int b=100;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(a+b);
+		
+		
+	}
+
+}
